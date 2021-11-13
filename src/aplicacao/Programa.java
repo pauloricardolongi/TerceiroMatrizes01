403165a6ba6abc0b7e18bea90d48fff89b35c510
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-13 01:00:44 -0300

    Programa matrizes

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,36 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int [][] mat = new int [n][n];
+		
+		for (int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Diagonal principal:");
+		for (int i=0; i<mat.length; i++) {
+			System.out.print(mat[i][i]+ " ");
+		}
+		System.out.println();
+		int conta = 0;
+		for (int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat[i].length; j++) {
+				if(mat[i][j] < 0) {
+					conta++;
+				}
+			}
+		}
+		System.out.println("Números Negativos:" + conta);
+		sc.close();
+
+	}
+
+}
